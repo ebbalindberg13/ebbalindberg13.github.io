e8522efd73f4c61b26067e7e6251361923483660
ebbalindberg13 <100775502+ebbalindberg13@users.noreply.github.com>
2023-01-04 14:57:37 +0100

    All files uploaded

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("I'm aliiiive");
+
+var projectGrid = document.getElementById("projectGrid");
+//var projectGridInfo = projectGrid.getBoundingClientRect();
+
+var sideNavLinks = document.querySelectorAll(".sideNav div");
+var section = document.querySelectorAll("section");
+
+// function activeSideNav() {
+// 	let length = section.length;
+// 	while(--length && window.scrollY < section[length].getBoundingClientRect().y + window.scrollY) {
+// 		sideNavLinks.forEach(item => item.classList.remove("active"));
+// 		sideNavLinks[(length - 1)].classList.add("active");
+// 	}
+// }
+// activeSideNav();
+// window.addEventListener("scroll", activeSideNav);
+
+var cards = document.querySelectorAll(".gridItem");
+
+const observer = new IntersectionObserver(
+	cards => {
+		cards.forEach(card => {
+			card.target.classList.toggle("visible", card.isIntersecting)
+			if (card.isIntersecting) observer.unobserve(card.target)
+		})
+	},
+	{
+		threshold: 0.3,
+	}
+	)
+
+cards.forEach(card => {
+	observer.observe(card);
+})
+
+var hamburgerMenu = document.querySelector('.hamburgerMenu');
+var navBar = document.querySelector('.navBar');
+var menuitems = document.querySelectorAll('.menuitem');
+var listMenu = document.querySelector('.listMenu');
+
+hamburgerMenu.addEventListener('click', function () {
+	hamburgerMenu.classList.toggle("active");
+	navBar.classList.toggle("a");
+	menuitems.forEach(item => {
+	item.classList.toggle("b");
+})
+	listMenu.classList.toggle("v");
+	console.log('hir')
+
+});
+
+menuitems.forEach(item => {
+	item.addEventListener('click', function() {
+		menuitems.forEach(item => {
+	item.classList.remove("b");
+		hamburgerMenu.classList.remove("active");
+		listMenu.classList.remove("v");
+	navBar.classList.remove("a");
+	})
+})
+
+})
