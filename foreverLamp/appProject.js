54fe6ee80a04cbf728ec0ce8dde7e8f43a50dc10
ebbalindberg13 <100775502+ebbalindberg13@users.noreply.github.com>
2023-01-04 18:26:39 +0100

    resp up

--- foreverLamp/appProject.js
+++ foreverLamp/appProject.js
@@ -90,3 +90,31 @@ function animateProgressBar() {
 
 }
 window.addEventListener("scroll", animateProgressBar);
+
+var hamburgerMenu = document.querySelector('.hamburgerMenu');
+var navBar = document.querySelector('.navBar');
+var menuitems = document.querySelectorAll('.menuitem');
+var listMenu = document.querySelector('.listMenu');
+
+hamburgerMenu.addEventListener('click', function () {
+	hamburgerMenu.classList.toggle("active");
+	navBar.classList.toggle("a");
+	menuitems.forEach(item => {
+	item.classList.toggle("b");
+})
+	listMenu.classList.toggle("v");
+	console.log('hir')
+
+});
+
+menuitems.forEach(item => {
+	item.addEventListener('click', function() {
+		menuitems.forEach(item => {
+	item.classList.remove("b");
+		hamburgerMenu.classList.remove("active");
+		listMenu.classList.remove("v");
+	navBar.classList.remove("a");
+	})
+})
+
+})
